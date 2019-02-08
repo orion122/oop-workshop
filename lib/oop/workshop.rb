2c03192d38397f1a1b89198503878a0c6b55e681
ramazan yaraliev <yaru@bk.ru>
2019-02-08 23:47:08 +0300

    implement class GeoData

--- lib/oop/workshop.rb
+++ lib/oop/workshop.rb
@@ -1,8 +1,29 @@
-require "oop/workshop/version"
+require 'oop/workshop/version'
+require 'net/http'
 
-module Oop
-  module Workshop
-    class Error < StandardError; end
-    # Your code goes here...
+module Oop::Workshop
+  class GeoData
+    URL = 'http://ip-api.com/json'
+
+    attr_reader :ip
+
+    def initialize(ip = nil)
+      @ip = ip
+    end
+
+    def get_url
+      @ip.nil? ? URL : "#{URL}/#{@ip}"
+    end
+
+    def get_data
+      uri = URI.parse(get_url)
+      request = Net::HTTP::Get.new(uri)
+
+      response = Net::HTTP.start(uri.host, uri.port) do |http|
+        http.request(request)
+      end
+
+      response.body
+    end
   end
 end
