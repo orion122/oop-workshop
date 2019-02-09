ab0aba1e04dcbf2fe2c878699d4ae9a40d7a9e73
ramazan yaraliev <yaru@bk.ru>
2019-02-09 16:16:49 +0300

    edit tests; pass fake http client

--- spec/oop/workshop_spec.rb
+++ spec/oop/workshop_spec.rb
@@ -1,22 +1,23 @@
 RSpec.describe Oop::Workshop do
+  class HttpClient
+    def self.get(uri)
+      { "city": 'Mountain View' }.to_json
+    end
+  end
+
   let(:ip) { '8.8.8.8' }
   let(:city) { 'Mountain View' }
-  let(:geo_data_class) { Oop::Workshop::GeoData }
-  let(:geo_data_pass_ip) { geo_data_class.new(ip) }
-  let(:geo_data_dont_pass_ip) { geo_data_class.new }
+  let(:geo_data) { Oop::Workshop::GeoData.new(http_client: HttpClient) }
 
   describe '#get_data' do
     context "don't pass ip" do
-      subject { geo_data_dont_pass_ip.get_data }
-      it { is_expected.to include('as', 'city', 'country', 'countryCode', 'isp', \
-                              'lat', 'lon', 'org', 'query', 'region', \
-                              'regionName', 'status', 'timezone', 'zip') }
+      subject { geo_data.get_data['city'] }
+      it { is_expected.to eq('Mountain View') }
     end
 
     context 'pass ip' do
-      subject { geo_data_pass_ip.get_data }
-      it { is_expected.to include(ip) }
-      it { is_expected.to include(city) }
+      subject { geo_data.get_data(ip: ip)['city'] }
+      it { is_expected.to eq('Mountain View') }
     end
   end
 end
