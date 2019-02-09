fe4fe4af1a783a6e93a18c3524957148b4d2ff2e
ramazan yaraliev <yaru@bk.ru>
2019-02-09 16:13:54 +0300

    don't store state

--- lib/oop/workshop.rb
+++ lib/oop/workshop.rb
@@ -6,69 +6,16 @@ module Oop::Workshop
   class GeoData
     URL = 'http://ip-api.com/json/'
 
-    attr_reader :data
+    attr_reader :http_client
 
     def initialize(http_client: Net::HTTP)
       @http_client = http_client
     end
 
-    def get_data(ip: '', http_client: @http_client)
+    def get_data(ip: '')
       uri = URI(URL + ip)
       response = http_client.get(uri)
-      @data = JSON.parse(response)
-      self
-    end
-
-    def get_city
-      data['city']
-    end
-
-    def get_country
-      data['country']
-    end
-
-    def get_country_code
-      data['countryCode']
-    end
-
-    def get_isp
-      data['isp']
-    end
-
-    def get_lat
-      data['lat']
-    end
-
-    def get_lon
-      data['lon']
-    end
-
-    def get_org
-      data['org']
-    end
-
-    def get_query
-      data['query']
-    end
-
-    def get_region
-      data['region']
-    end
-
-    def get_region_name
-      data['regionName']
-    end
-
-    def get_status
-      data['status']
-    end
-
-    def get_timezone
-      data['timezone']
-    end
-
-    def get_zip
-      data['zip']
+      JSON.parse(response)
     end
   end
 end
